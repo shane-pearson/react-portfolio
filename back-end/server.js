be841dbac491a55e400734cc024449c67d572fa7
Pearson <spearson@lwsd.org>
2023-05-25 10:02:50 -0700

    cors stuff

--- back-end/server.js
+++ back-end/server.js
@@ -4,16 +4,11 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
