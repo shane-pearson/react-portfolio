9f94b9ff95bec955a0a695a390b26896c304a9ec
Pearson <spearson@lwsd.org>
2023-05-26 11:33:48 -0700

    changed fetch

--- back-end/server.js
+++ back-end/server.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
-app.use(cors({ origin: true }));
+app.use(cors());
 app.use(express.json());
-app.use("/contact", router);
+app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
