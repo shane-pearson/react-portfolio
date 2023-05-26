087dac3615b02d67238ced508c78d6aa6dc643a2
Pearson <spearson@lwsd.org>
2023-05-26 12:41:38 -0700

    more cors fixing

--- back-end/server.js
+++ back-end/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://main--statuesque-dragon-8a2ce7.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
